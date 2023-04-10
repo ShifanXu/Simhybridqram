3976a173cd686576b9bef07aef0c9c15fb700e5f
ShifanXu <103957936+ShifanXu@users.noreply.github.com>
2023-04-10 14:31:36 -0400

    modify bash

--- my_job.sh
+++ my_job.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-#SBATCH --partition=devel
-#SBATCH --job-name=my_conda_job
-#SBATCH --cpus-per-task 4
-#SBATCH --mem-per-cpu=4000
+#SBATCH --job-name=my_job
+#SBATCH --time=2:00:00
+#SBATCH --mem=4G
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --out="/gpfs/gibbs/project/ding/sx86/234010test/slurm_jobs/%a.jobout"
+#SBATCH --error="/gpfs/gibbs/project/ding/sx86/234010test/slurm_jobs/%a.joberror"
 
 
 module load miniconda
 
 conda activate qiskitenv3
 python 1layer.py
+
