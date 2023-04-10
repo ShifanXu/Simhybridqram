6b8c04abec93cdf67a0cacd82489fcb59fa86ad0
ShifanXu <103957936+ShifanXu@users.noreply.github.com>
2023-04-10 15:55:49 -0400

    modification

--- my_job.sh
+++ my_job.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=my_job
-#SBATCH --time=2:00:00
-#SBATCH --mem=4G
-#SBATCH --mail-type=ALL
+#SBATCH --job-name=my_job1
+#SBATCH --partition=day
+#SBATCH --time=4:00:00
+#SBATCH --mem=160G
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --out="/gpfs/gibbs/project/ding/sx86/234010test/slurm_jobs/%a.jobout"
-#SBATCH --error="/gpfs/gibbs/project/ding/sx86/234010test/slurm_jobs/%a.joberror"
+#SBATCH --mail-type=ALL
+#SBATCH --ntasks=1S
+#SBATCH --cpus-per-task=48
+#SBATCH --out="/gpfs/gibbs/project/ding/sx86/234010test/a.jobout"
+#SBATCH --error="/gpfs/gibbs/project/ding/sx86/234010test/a.joberror"
 
 
 module load miniconda
 
 conda activate qiskitenv3
-python 1layer.py
-
+python reduced_2layer.py
